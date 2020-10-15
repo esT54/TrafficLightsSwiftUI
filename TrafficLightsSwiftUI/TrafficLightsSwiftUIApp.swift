fbef06e4617fd22455ca6a99eaac788d31a74e03
est54 <smirnyagin.semen@gmail.com>
2020-10-15 10:28:34 +0700

    Initial Commit

---
+++ TrafficLightsSwiftUI/TrafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUIApp.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Семен on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
